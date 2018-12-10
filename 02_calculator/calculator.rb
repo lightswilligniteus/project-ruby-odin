84f00a7915cafe7063ce75976a57d09aabd2cd56
kdaya <kdayathegamer@gmail.com>
2018-12-10 14:58:07 +1100

    complete calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,26 @@
 #write your code here
+def add(a, b)
+  a + b
+end
+
+def subtract(a,b)
+  a - b
+end
+
+def sum(arr)
+  a = 0
+  arr.each {|i| a += i}
+  a
+end
+
+def multiply(a, b)
+  a * b
+end
+
+def power(a, b)
+  a ** b
+end
+
+def factorial(a, b)
+  a % b
+end
