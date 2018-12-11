aa3f509ff5ee208732614ceccd7b82cfc6c558e1
kdaya <kdayathegamer@gmail.com>
2018-12-11 13:18:07 +1100

    basic logic working

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -2,7 +2,14 @@ class Book
   attr_reader :title
 
   def title=(new_title)
-    new_title = new_title.split.map { |i| i.capitalize }.join(' ')
+    new_title = new_title.capitalize
+    new_title = new_title.split.map { |i|
+      if i != "and" && i != "in" && i != "of" && i != "the" && i != "a" && i != "an"
+        i.capitalize
+      else
+        i
+      end
+      }.join(' ')
     @title = new_title
   end
 
